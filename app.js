be3429792d919398db52577a40add72b969852c0
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-23 17:32:11 -0600

    added public methods in the UICtrl to grab the input field's values. from the DOM. Made private obj in UICtrl to make the references to dom classes and id's modular acrossed controllers

--- app.js
+++ app.js
@@ -7,6 +7,25 @@ var budgetController = (function () {
 
 //UI CONTROLLER
 var UIController = (function () {
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // should be inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function () {
+      return DOMStrings;
+    }
+  };
 
 })();
 
@@ -14,9 +33,12 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings();
+
   var ctrlAddItem = function () {
     //1. Get the field input data
-
+    var input = UICtrl.getInput();
+    console.log(input);
     //2. Add the item to the budgetCtrl
 
     //3. Add the item to the UI
@@ -24,11 +46,10 @@ var controller = (function (budgetCtrl, UICtrl) {
     //4. Calculate budget
 
     //5, Display the budget on the UI
-    console.log('itworks');
   };
 
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 
   document.addEventListener('keypress', function (event) {
