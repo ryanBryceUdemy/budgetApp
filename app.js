cd0d7185d608ced7910dedfd56fe6e884b8650c4
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-23 16:24:57 -0600

    setting up event listeners, working on keeping code dry.

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
+//BUDGET CONTROLLER
 var budgetController = (function () {
-  var x = 23;
-  function add(a) {
-    return x + a;
-  }
-  return {
-    publicTest: function (b) {
-        return add(b);
-    }
-  };
+
+  //SOME CODE
+
 })();
 
-
+//UI CONTROLLER
 var UIController = (function () {
 
 })();
 
+//GLOBAL APP CONTROLLER
 
 var controller = (function (budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function () {
-      console.log(z);
-    }
+
+  var ctrlAddItem = function () {
+    //1. Get the field input data
+
+    //2. Add the item to the budgetCtrl
+
+    //3. Add the item to the UI
+
+    //4. Calculate budget
+
+    //5, Display the budget on the UI
+    console.log('itworks');
   };
+
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+
 })(budgetController, UIController);
