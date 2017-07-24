3619156be387ac492ff2d39c22c16fc3830b08b1
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-23 22:57:12 -0600

    setting up ui method. done for the night

--- app.js
+++ app.js
@@ -66,6 +66,14 @@ var UIController = (function () {
         value: document.querySelector(DOMStrings.inputValue).value
       };
     },
+    addListItem: function () {
+      var html;
+      //create HTML string with placeholder text
+      html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      //replace the placeholder text with actuall data
+
+      //insert the HTML into the DOM
+    },
     getDOMStrings: function () {
       return DOMStrings;
     }
