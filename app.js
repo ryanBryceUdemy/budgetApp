f0b8ee64a640abc493037738878690559e7bb2d4
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-24 13:25:45 -0600

    set up public method in UI controller to append HTML elements to DOM. this methodd creates an HTML template. i then update the template with the data coming from my apps data structure using the string replace method. this then gets added to the DOM in the correct income and expense fields.

--- app.js
+++ app.js
@@ -55,7 +55,9 @@ var UIController = (function () {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {
@@ -66,13 +68,28 @@ var UIController = (function () {
         value: document.querySelector(DOMStrings.inputValue).value
       };
     },
-    addListItem: function () {
-      var html;
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
       //create HTML string with placeholder text
-      html = '<div class="item clearfix" id="income-0"><div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      if (type === 'inc') {
+        element = DOMStrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }else if (type === 'exp') {
+        element = DOMStrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
       //replace the placeholder text with actuall data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
 
       //insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
+    clearFields: function () {
+
     },
     getDOMStrings: function () {
       return DOMStrings;
@@ -101,7 +118,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     //2. Add the item to the budgetCtrl
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add the item to the UI
-
+    UICtrl.addListItem(newItem, input.type);
     //4. Calculate budget
 
     //5, Display the budget on the UI
