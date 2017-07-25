b5337e3d80df533bb4facec752954030f361d4e3
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-25 12:24:47 -0600

    added eventlistener on the container for all evlements both income and expense. i did this because i wanted to use event delegation to keep code dry and to listen to the button click to bubble up. used the event.target to traverse the DOM to get the id of the element we clicked on. then i used the String.split method to get an array of the type (income/expense) and the id. now i'm set to delete the item from data structure and update the ui.

--- app.js
+++ app.js
@@ -96,7 +96,8 @@ var UIController = (function () {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   };
 
   return {
@@ -112,10 +113,10 @@ var UIController = (function () {
       //create HTML string with placeholder text
       if (type === 'inc') {
         element = DOMStrings.incomeContainer;
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }else if (type === 'exp') {
         element = DOMStrings.expensesContainer;
-        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
       //replace the placeholder text with actuall data
@@ -166,6 +167,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
   };
   var updateBudget = function () {
     //1. Calculate budget
@@ -191,6 +193,25 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
   };
+
+  var ctrlDeleteItem = function(event) {
+    var itemID, splitID, type, ID;
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. delete the item from the data structure;
+
+      // 2. Delete the item from the ui;
+
+      // 3. update and show the new budget;
+
+    }
+  };
+
   return {
     init: function () {
       UICtrl.displayBudget({
