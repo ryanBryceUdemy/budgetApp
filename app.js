106c2b96802e2ad5d3c67ebd5d922a13449ec6af
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-25 20:29:39 -0600

    added method in UICtrl that will call the Date constructor and i then grab the mzero index month, and then the year. i made an array of months and then using baracket notation i used the zero indexed month to get the proper month out of the array. i then assign the textContent equl to the month and year.

--- app.js
+++ app.js
@@ -130,7 +130,8 @@ var UIController = (function () {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   };
 
   var formatNumber = function (num, type) {
@@ -218,6 +219,15 @@ var UIController = (function () {
         }
       });
     },
+    displayMonth: function () {
+      var now, year, month, months;
+
+      now = new Date();
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
     getDOMStrings: function () {
       return DOMStrings;
     }
@@ -298,13 +308,15 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   return {
     init: function () {
+      console.log('app is running');
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
         totalExp: 0,
         percentage: -1
       });
-        console.log('app is running');
+
         setupEventListeners();
     }
   };
